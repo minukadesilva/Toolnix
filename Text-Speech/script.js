bab21602953b6ed39a0aac9c28fc488a87bc297b
Minuka De Silva <100896047+minukadesilva@users.noreply.github.com>
2025-08-01 22:08:34 +0530

    Add files via upload

--- Text-Speech/script.js
+++ Text-Speech/script.js
@@ -1,126 +1,57 @@
-//Random Quotes Api URL
-const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
-const quoteSection = document.getElementById("quote");
-const userInput = document.getElementById("quote-input");
-let quote = "";
-let time = 60;
-let timer = "";
-let mistakes = 0;
+const textarea = document.querySelector("textarea"),
+voiceList = document.querySelector("select"),
+speechBtn = document.querySelector("button");
 
-//Display random quotes
-const renderNewQuote = async () => {
-  //Fetch contents from url
-  const response = await fetch(quoteApiUrl);
+let synth = speechSynthesis,
+isSpeaking = true;
 
-  //Store response
-  let data = await response.json();
+voices();
 
-  //Access quote
-  quote = data.content;
-
-  //Array of characters in the quote
-  let arr = quote.split("").map((value) => {
-    //wrap the characters in a span tag
-    return "<span class='quote-chars'>" + value + "</span>";
-  });
-  //join array for displaying
-  quoteSection.innerHTML += arr.join("");
-};
-
-//Logic for comparing input words with quote
-userInput.addEventListener("input", () => {
-  let quoteChars = document.querySelectorAll(".quote-chars");
-  //Create an arrat from received span tags
-  quoteChars = Array.from(quoteChars);
-
-  //array of user input characters
-  let userInputChars = userInput.value.split("");
-
-  //loop through each character in quote
-  quoteChars.forEach((char, index) => {
-    //Check if char(quote character) = userInputChars[index](input character)
-    if (char.innerText == userInputChars[index]) {
-      char.classList.add("success");
+function voices(){
+    for(let voice of synth.getVoices()){
+        let selected = voice.name === "Google US English" ? "selected" : "";
+        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+        voiceList.insertAdjacentHTML("beforeend", option);
     }
-    //If user hasn't entered anything or backspaced
-    else if (userInputChars[index] == null) {
-      //Remove class if any
-      if (char.classList.contains("success")) {
-        char.classList.remove("success");
-      } else {
-        char.classList.remove("fail");
-      }
-    }
-    //If user enter wrong character
-    else {
-      //Checks if we alreasy have added fail class
-      if (!char.classList.contains("fail")) {
-        //increment and display mistakes
-        mistakes += 1;
-        char.classList.add("fail");
-      }
-      document.getElementById("mistakes").innerText = mistakes;
-    }
-    //Returns true if all the characters are entered correctly
-    let check = quoteChars.every((element) => {
-      return element.classList.contains("success");
-    });
-    //End test if all characters are correct
-    if (check) {
-      displayResult();
-    }
-  });
-});
-
-//Update Timer on screen
-function updateTimer() {
-  if (time == 0) {
-    //End test if timer reaches 0
-    displayResult();
-  } else {
-    document.getElementById("timer").innerText = --time + "s";
-  }
 }
 
-//Sets timer
-const timeReduce = () => {
-  time = 60;
-  timer = setInterval(updateTimer, 1000);
-};
+synth.addEventListener("voiceschanged", voices);
 
-//End Test
-const displayResult = () => {
-  //display result div
-  document.querySelector(".result").style.display = "block";
-  clearInterval(timer);
-  document.getElementById("stop-test").style.display = "none";
-  userInput.disabled = true;
-  let timeTaken = 1;
-  if (time != 0) {
-    timeTaken = (60 - time) / 100;
-  }
-  document.getElementById("wpm").innerText =
-    (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
-  document.getElementById("accuracy").innerText =
-    Math.round(
-      ((userInput.value.length - mistakes) / userInput.value.length) * 100
-    ) + " %";
-};
+function textToSpeech(text){
+    let utterance = new SpeechSynthesisUtterance(text);
+    for(let voice of synth.getVoices()){
+        if(voice.name === voiceList.value){
+            utterance.voice = voice;
+        }
+    }
+    synth.speak(utterance);
+}
 
-//Start Test
-const startTest = () => {
-  mistakes = 0;
-  timer = "";
-  userInput.disabled = false;
-  timeReduce();
-  document.getElementById("start-test").style.display = "none";
-  document.getElementById("stop-test").style.display = "block";
-};
-
-window.onload = () => {
-  userInput.value = "";
-  document.getElementById("start-test").style.display = "block";
-  document.getElementById("stop-test").style.display = "none";
-  userInput.disabled = true;
-  renderNewQuote();
-};
+speechBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    if(textarea.value !== ""){
+        if(!synth.speaking){
+            textToSpeech(textarea.value);
+        }
+        if(textarea.value.length > 80){
+            setInterval(()=>{
+                if(!synth.speaking && !isSpeaking){
+                    isSpeaking = true;
+                    speechBtn.innerText = "Convert To Speech";
+                }else{
+                }
+            }, 500);
+            if(isSpeaking){
+                synth.resume();
+                isSpeaking = false;
+                speechBtn.innerText = "Pause Speech";
+            }else{
+                synth.pause();
+                isSpeaking = true;
+                speechBtn.innerText = "Resume Speech";
+            }
+        }else{
+            speechBtn.innerText = "Convert To Speech";
+        }
+    }
+});
